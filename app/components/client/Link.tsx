f26a4c1172daf75285e5c7b5d6673182ea74e828
Kran67 <lopesmiguel80@gmail.com>
2025-12-17 12:52:38 +0100

    Composant Lien

---
+++ app/components/client/Link.tsx
@@ -0,0 +1,43 @@
+'use client'
+
+import { useRouter } from "next/navigation";
+import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
+
+
+interface LinkProps {
+    text: string;
+    url?: string;
+    disabled?: boolean;
+}
+
+export default function Link({ text, url = "#", disabled = false }: LinkProps) {
+    const router: AppRouterInstance = useRouter();
+
+    const handleClick = () => {
+        router.push(url);
+    };
+
+    const classNames = [
+        "link",
+        "inline-flex",
+        "items-center",
+        "justify-center",
+        "whitespace-nowrap",
+        "text-base",
+        disabled ? "text-(--grey-400)" : "text-(--dark-orange)",
+        "body-s",
+        disabled ? "cursor-not-allowed" : "cursor-pointer",
+        "underline",
+        disabled ? "" : "hover:text-(--grey-950)",
+        disabled ? "disabled" : "",
+
+    ].join(" ")
+
+    return (
+        <a href={url}
+            className={classNames}
+            onClick={handleClick}>
+            {text}
+        </a>
+    );
+}
